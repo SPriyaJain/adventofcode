c5303dfe3b6ffe5322bdbd628000b1e3b38797d8
Samta Priya Jain <priyaj127@hotmail.com>
2016-12-31 14:27:40 -0500

    Small change in style

--- aoc5.py
+++ aoc5.py
@@ -2,14 +2,14 @@ import hashlib
 door = "cxdnnyjw"
 
 # Part 1
-# code = ""
-# i = 0
-# while len(code) < 8:
-#     mhash = hashlib.md5(door + str(i)).hexdigest()
-#     if mhash[:5] == "00000":
-#         code += str(mhash[5])
-#     i += 1
-# print "Part 1 answer: " + code
+code = ""
+i = 0
+while len(code) < 8:
+    mhash = hashlib.md5(door + str(i)).hexdigest()
+    if mhash[:5] == "00000":
+        code += str(mhash[5])
+    i += 1
+print "Part 1 answer: " + code
 
 # Part 2
 code = "........"
